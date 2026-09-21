0664821f6fed778bb4f4998dfd186a06fcc2062e
talchao <benjamin.mossor@gmail.com>
2026-09-21 09:55:54 -0500

    Initial commit

---
+++ future_value.js
@@ -0,0 +1,11 @@
+var calculateClick = function () {
+// Your code goes here.
+
+
+}
+
+
+$(document).ready(function() {
+    $("#calculate").click(calculateClick);
+    $("#investment").focus();
+});
